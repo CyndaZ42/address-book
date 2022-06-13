25cf086b56922c22546637d430834b853449144c
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-13 11:30:39 -0700

    add displayContactDetails
    Co-Authored-By: zbl14 <105818830+zbl14@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,16 @@ Contact.prototype.fullName = function() {
 //User Interface Logic
 let addressBook = new AddressBook();
 
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -53,6 +63,6 @@ $(document).ready(function() {
     const inputtedPhoneNumber = $("input#new-phone-number").val();
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
   });
 });
