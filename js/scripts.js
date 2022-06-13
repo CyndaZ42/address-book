9032bb9169875f5801b19be2f7f07a526092cabd
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-13 09:42:05 -0700

    add id logic to address book
    Co-Authored-By: zbl14 <105818830+zbl14@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,32 @@
 
 function AddressBook() {
   this.contacts = {};
+  this.currentId = 0;
 }
 
 AddressBook.prototype.addContact = function(contact) {
-  this.contacts[contact.firstName] = contact;
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
 };
 
 // Business Logic for Contacts ---------
