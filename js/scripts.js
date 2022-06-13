b1334e9c63c3f14856b368642f1fe964362806dd
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-13 09:31:20 -0700

    add address book and contacts logic
    Co-authored-by: Zhibin Liang  <ifthereisoneday@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Business Logic for AddressBook ---------
+
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+// Business Logic for Contacts ---------
+
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
